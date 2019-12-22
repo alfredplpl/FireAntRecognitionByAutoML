b3c70185f619e43dbc54c43c7dcf13b7057ae63e
Yasunori Ozaki <ozaki.yasunori@outlook.com>
2019-12-22 18:27:02 +0900

    Update predict

--- predictByAutoKeras.py
+++ predictByAutoKeras.py
@@ -3,11 +3,13 @@ __version__ = "0.0.1"
 __license__ = "Apache License 2.0"
 
 import imageio
+import numpy as np
 from autokeras.utils import pickle_from_file
 
 import Settings
 
 X=imageio.imread(Settings.TEST_PATH)
-clf = pickle_from_file("models/cllasifier")
+X=X[:,:,:,np.newaxis]
+clf = pickle_from_file(Settings.MODEL_PATH)
 result = clf.predict(X)
 print(result)
