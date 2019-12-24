0475df3dc30cae2e912e999ea09cd9c367a7d067
Yasunori Ozaki <ozaki.yasunori@outlook.com>
2019-12-24 12:05:10 +0900

    Update README about model

--- predictByAutoKeras.py
+++ predictByAutoKeras.py
@@ -2,14 +2,16 @@ __author__ = "Alfred Increment"
 __version__ = "0.0.1"
 __license__ = "Apache License 2.0"
 
+import sys
 import imageio
 import numpy as np
 from autokeras.utils import pickle_from_file
 
 import Settings
 
-X=imageio.imread(Settings.TEST_PATH)
-X=X[np.newaxis,:,:,:]
-clf = pickle_from_file(Settings.MODEL_PATH)
-result = clf.predict(X)
-print(result)
+if __name__ == "__main__":
+    X=imageio.imread(Settings.TEST_PATH)
+    X=X[np.newaxis,:,:,:]
+    clf = pickle_from_file(Settings.MODEL_PATH)
+    result = clf.predict(X)
+    print(result)
--- trainByAutoKeras.py
+++ trainByAutoKeras.py
@@ -3,12 +3,43 @@ __version__ = "0.0.1"
 __license__ = "Apache License 2.0"
 
 import autokeras as ak
-from autokeras.image.image_supervised import load_image_dataset
+from sklearn.model_selection import train_test_split
+
+from keras.utils import plot_model
+from keras.models import load_model
+import numpy as np
+import pandas as pd
+import cv2
+import os
 
 import Settings
 
-X, y = load_image_dataset(csv_file_path="trainForAutokeras.csv",images_path=Settings.DATASET_DIR)
+dataList = pd.read_csv("trainForAutoPyTorch.csv", header=0)
+
+X=[]
+y=[]
+for index, row in dataList.iterrows():
+    #center cropping
+    img=cv2.imread(os.path.join(Settings.DATASET_DIR, row["File Name"]))
+    w=img.shape[1]
+    h=img.shape[0]
+    edge=np.min(img.shape)
+    img=img[(h-edge)//2:(h+edge)//2,(w-edge)//2:(w+edge)//2,:]
+    img=cv2.resize(img,(224,224 ))
+    X.append(img)
+
+    y.append(row["Label"])
+X=np.array(X,dtype=np.float64)/255.
+y=np.array(y,dtype=np.int64)
+
+X_train, X_test, y_train, y_test=train_test_split(X, y, test_size=0.1)
 clf = ak.ImageClassifier(path=Settings.TMP_PATH,augment=True,verbose=True)
-clf.fit(X, y,time_limit=60*60)
+clf.fit(X_train, y_train,time_limit=24*60*60)
+clf.final_fit(X_train, y_train, X_test, y_test, retrain=True)
+results = clf.evaluate(X_test, y_test)
+print(results)
 
 clf.export_autokeras_model(Settings.MODEL_PATH)
+
+# References
+# https://qiita.com/hideki/items/24f6c776828ac918a109
